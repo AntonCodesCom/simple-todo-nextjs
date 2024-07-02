a165306f0a838df053bb0eaad3768f236294d6ec
Anton <antoncodes@outlook.com>
2024-07-02 12:32:40 +0300

    mui theme roboto font

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,5 +1,14 @@
 import { colors, createTheme } from '@mui/material';
+import { Roboto } from 'next/font/google';
 
+// font
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+  display: 'swap',
+});
+
+// theme
 const theme = createTheme({
   components: {
     MuiContainer: {
@@ -21,6 +30,9 @@ const theme = createTheme({
     },
   },
   spacing: (x: number) => `${x}rem`,
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
 });
 
 export default theme;
